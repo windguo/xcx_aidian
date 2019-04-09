a10934a053d2b2b1b9a3b8386053e7614f29b4a4
windguo <webmaster@windguo.com>
2019-04-10 07:24:47 +0800

    0724

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,8 +4,7 @@ var app = getApp()
 Page({
   onShareAppMessage: function (res) {
 		return {
-			title: '爱小程序商店欢迎您...',
-			imageUrl:'/images/indexPic.jpg',
+			title: '爱店(爱小程序商店)欢迎您...',
 			path: '/pages/index/index',
 			success: (res) => {
 				wx.showToast({
@@ -45,6 +44,7 @@ Page({
     _windowWidth: wx.getSystemInfoSync().windowWidth,
     contentArray: [],
 		eliteArray:[],
+    newArray: [],
 		xiaochengxuClassArray:[],
 		xiaoyouxiClassArray:[]
   },
@@ -66,10 +66,26 @@ Page({
 						eliteArray: json.data.result
 					})
 				}
-				wx.hideLoading()
 			}
 		})
 	},
+  getNewData: function () {
+    let that = this;
+    wx.request({
+      url: 'https://www.yishuzi.com.cn/shop_xiaochengxu_api/?getJson=column&classid=9999&pageSize=8',
+      method: 'GET',
+      dataType: 'json',
+      success: (json) => {
+        console.log('---====eeee==------', json.data.result)
+        if (json.data.status == 1) {
+          that.setData({
+            newArray: json.data.result
+          })
+        }
+        wx.hideLoading()
+      }
+    })
+  },
   searchPage: function () {
     wx.navigateTo({
       url: '../search/search'
@@ -89,7 +105,6 @@ Page({
 						xiaochengxuClassArray: json.data.result
 					})
 				}
-				wx.hideLoading()
 			}
 		})
 	},
@@ -107,13 +122,11 @@ Page({
 						xiaoyouxiClassArray: json.data.result
 					})
 				}
-				wx.hideLoading()
 			}
 		})
 	},
   onLoad: function (options) {
     wx.showLoading({title:'加载中...'})
-
     // 扫码进入的判断开始
     const _scene = options.scene
     console.log('_scene_scene', _scene)
@@ -131,7 +144,9 @@ Page({
 		this.getEliteData();
 		// 获取小程序分类
 		this.getXiaochengxuClass();
-		// 获取小youxi分类
+		// 获取小游戏分类
 		this.getXiaoyouxiClass();
+    // 获取最新小程序
+    this.getNewData();
   }
 })
